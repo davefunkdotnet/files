1d04b9c61ef1323143656897bd7455e257687efc
Dave <davefunk135@gmail.com>
2018-02-24 21:45:15 -0800

    express server up serving index.html

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
 
 //Express server listener > > 
-app.listen(PORT, function (){
+app.listen(PORT, function () {
     console.log(`Application listening on port ${PORT}`);
-})
+});
