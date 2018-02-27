fc46683b6e0b356c1bcbf35df038687944d0acec
Dave <davefunk135@gmail.com>
2018-02-26 23:30:38 -0800

    stylesheet added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var PORT = process.env.PORT || 5000;
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 })
