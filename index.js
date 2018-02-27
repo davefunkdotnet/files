f1af33750a63148fa027da9102b676a00f0e7af6
Dave Funk <Dave@Daves-MacBook-Pro.local>
2018-02-26 19:35:12 -0800

    updates for testing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
