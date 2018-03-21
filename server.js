f12f7a2261a2888148bf887304d19c932df12382
Dave <davefunk135@gmail.com>
2018-03-21 01:13:51 -0700

    refactored server.js, added route controller, initialized handlebars

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+//init express
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+//handlebars
 const handlebars = require('express-handlebars');
 app.engine("handlebars", handlebars({defaultLayout: "main"}));
 app.set("view_engine", "handlebars");
+//body parser
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//routes
+const routeHandling = require('./controllers/route_controller.js');
 app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+app.use(routeHandling);
 
 //Express server listener > > 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, function () {
     console.log(`Application listening on port ${PORT}`);
 });
