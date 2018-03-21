a013ab6e2fd70c3f97aeb1fb00a1005db3d26e63
Dave <davefunk135@gmail.com>
2018-03-21 00:34:39 -0700

    initialized sequelize

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const handlebars = require('express-handlebars');
 app.engine("handlebars", handlebars({defaultLayout: "main"}));
 app.set("view_engine", "handlebars");
